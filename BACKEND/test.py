682404c0127738ec86602140219d1ab046215e05
r-adaR <phillip.peterson873@gmail.com>
2025-01-12 15:49:39 -0800

    OpenCV install and test.

---
+++ BACKEND/test.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+image = np.zeros((512, 512, 3), np.uint8)
+
+cv2.line(image, (0, 0), (511, 511), (0, 255, 0), 5)
+
+cv2.rectangle(image, (384, 0), (510, 128), (0, 0, 255), 3)
+
+cv2.imshow('Image', image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A program that supports a hybrid model of tabletop games.
+
+pip install opencv-python
