e5dc491b05fdc9bcd0bbb5f54080854631a08f88
derkyazn <131503815+derkyazn@users.noreply.github.com>
2025-01-28 11:47:09 -0800

    Update test.py
    Testing cv2 camera access

--- BACKEND/test.py
+++ BACKEND/test.py
@@ -11,3 +11,26 @@ cv2.imshow('Image', image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+# Open Camera
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+    # Display the frame in a window
+    cv2.imshow('Camera', frame)
+
+    # Exit if the user presses 'q'
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the camera and close the window
+cap.release()
+cv2.destroyAllWindows()
+
+
+
