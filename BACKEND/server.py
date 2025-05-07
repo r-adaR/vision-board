1943c51e582e313f299b22b69f7c7c7382d93ca7
derkyazn <131503815+derkyazn@users.noreply.github.com>
2025-05-07 13:02:11 -0700

    Created framework for backend server

--- BACKEND/server.py
+++ BACKEND/server.py
@@ -1,4 +1,7 @@
 import socket
+import cv2 as cv
+import base64
+from MAIN_METHOD.VisionBoard import vision_board_reader
 
 # Establish localhost and port.
 HOST = "127.0.0.1"
@@ -10,15 +13,43 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen(1)
     conn, addr = s.accept()
 
+    # open camera
+    camera = cv.VideoCapture(1)
+    if not camera.isOpened():
+        raise Exception("Could not open camera.")
+    
+
+
+    
+
+
     with conn:
         print('Connected by', addr)
         while True:
             # Echo data received back to the client (TODO: Change to send board data).
+
+            # read frame from camera
+            ret, frame = camera.read()
+            if not ret:
+                break
+
+
             data = conn.recv(1024)
             if data == b"SCF":
+                img_encode = cv.imencode('.jpg', frame)[1].tobytes()
+                base64_bytes = base64.b64encode(img_encode)
+                # print(base64_bytes)
+
                 break
             if data == b"RGS":
+                boardState = vision_board_reader(frame).tobytes()
+                conn.sendall(boardState)
+
                 break
             if not data:
                 break
-            conn.sendall(data)
+            # else:
+            #     conn.sendall("incorrect protocol?")
+
+
+    camera.release()
