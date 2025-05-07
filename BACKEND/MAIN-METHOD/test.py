f2f5ac5be66106586a55f1cdbab61a58a38bbd53
r_adaR <phillip.peterson873@gmail.com>
2025-05-07 11:18:55 -0700

    Integrate computer vision files.

--- BACKEND/MAIN-METHOD/test.py
+++ BACKEND/MAIN-METHOD/test.py
@@ -2,10 +2,12 @@ from VisionBoard import vision_board_reader
 import cv2 as cv
 
 if __name__ == '__main__':
-    test_image = cv.imread("training_data/Hard/hard_385.png")
-    board = vision_board_reader(test_image)
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == '':
-                board[i][j] = ' '
-    print(board)
+    for i in range(10, 19):
+        print(f"easy_{i}.png")
+        test_image = cv.imread(f"training_data/Easy/easy_{i}.png")
+        board = vision_board_reader(test_image)
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == '':
+                    board[i][j] = ' '
+        print(board)
