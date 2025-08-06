671239a498735186ecd6ceb7c762629db1a18006
derkyazn <131503815+derkyazn@users.noreply.github.com>
2025-08-05 17:38:05 -0700

    replace all coroutines with async

--- UNITY/Assets/Scripts/Game Logic/GameFlow.cs
+++ UNITY/Assets/Scripts/Game Logic/GameFlow.cs
@@ -55,36 +55,82 @@ public class GameFlow : MonoBehaviour
     }
 
     private float clock = 0;
+
     private void Update()
     {
         if (!readingCoroutineActive) clock += Time.deltaTime; // only increase clock counter if we're currently not confirming the board state
-        if (clock > 1) // every second, see if the read board state is different
+        if (clock > 1f) // every second, see if the read board state is different
         {
             clock = 0;
             if (!readingCoroutineActive && !checkingBoardChange)
-                StartCoroutine(CheckForBoardChange());
+            {
+                _ = CheckForBoardChangeAsync();
+            }
         }
     }
 
-    private IEnumerator CheckForBoardChange()
+
+    //private IEnumerator CheckForBoardChange()
+    //{
+    //    checkingBoardChange = true;
+
+    //    //run getBoardStateAsync as a task
+    //    Task<Side[,]> task = network_instance.GetBoardStateAsync();
+
+    //    //while getBoardStateAsync is still running wait here for another frame --> yield time back to the main thread (doesn't block)
+    //    while (!task.IsCompleted)
+    //        yield return null;
+
+    //    Side[,] newBoard = task.Result;
+
+
+    //    bool newBoardIsSame = AreBoardsEqual(newBoard, game_instance.board);
+    //    if (newBoard != null && !newBoardIsSame)
+    //    {
+    //        // start the confirmation process
+    //        StartCoroutine(ConfirmBoardState(newBoard));
+    //    }
+    //    else if (newBoard == null)
+    //    {
+    //        Debug.LogWarning("Board could not be read");
+    //    }
+    //    else
+    //    {
+    //        board_visuals.ClearErrors();
+    //        if (illegalMoveIndicator.localPosition.y > 0) // if illegal move indicator is shown on screen
+    //        {
+    //            illegalMoveIndicator.DOLocalMoveY(-50, 0.3f).SetEase(Ease.InExpo);
+    //        }
+    //    }
+    //    checkingBoardChange = false;
+    //}
+
+
+    
+
+    private async Task CheckForBoardChangeAsync()
     {
+        if (checkingBoardChange) return;
         checkingBoardChange = true;
 
-        //run getBoardStateAsync as a task
-        Task<Side[,]> task = network_instance.GetBoardStateAsync();
-
-        //while getBoardStateAsync is still running wait here for another frame --> yield time back to the main thread (doesn't block)
-        while (!task.IsCompleted)
-            yield return null;
-
-        Side[,] newBoard = task.Result;
-
+        Side[,] newBoard;
+        try
+        {
+            // will wait here until the GetBoardStateAsync() function returns --> while waiting, will yield back to main thread (does not block)
+            newBoard = await network_instance.GetBoardStateAsync();
+        }
+        catch (Exception e)
+        {
+            checkingBoardChange = false;
+            return;
+        }
 
         bool newBoardIsSame = AreBoardsEqual(newBoard, game_instance.board);
         if (newBoard != null && !newBoardIsSame)
         {
             // start the confirmation process
-            StartCoroutine(ConfirmBoardState(newBoard));
+            // wait here until ConfirmBoardStateAsync() returns --> does not block main thread
+            await ConfirmBoardStateAsync(newBoard);
         }
         else if (newBoard == null)
         {
@@ -98,8 +144,10 @@ public class GameFlow : MonoBehaviour
                 illegalMoveIndicator.DOLocalMoveY(-50, 0.3f).SetEase(Ease.InExpo);
             }
         }
+
         checkingBoardChange = false;
     }
+
 
 
     /// <summary>
@@ -108,34 +156,197 @@ public class GameFlow : MonoBehaviour
     /// </summary>
     /// <param name="board"></param>
     /// <returns></returns>
-    private IEnumerator ConfirmBoardState(Side[,] _board)
+    //private IEnumerator ConfirmBoardState(Side[,] _board)
+    //{
+    //    readingCoroutineActive = true;
+
+    //    // confirm board state
+    //    int correct = 0;
+    //    int nulls = 0;
+    //    for (int i = 0; i < NUMBER_OF_SCANS; i++)
+    //    {
+
+    //        //run getBoardStateAsync as a task
+    //        Task<Side[,]> task = network_instance.GetBoardStateAsync();
+
+    //        //while getBoardStateAsync is still running wait here for another frame --> yield time back to the main thread (doesn't block)
+    //        while (!task.IsCompleted)
+    //            yield return null;
+    //        Side[,] newScan = task.Result;
+
+
+    //        if (newScan == null) nulls++;
+    //        else if (AreBoardsEqual(newScan, _board)) correct++;
+
+    //        print($"nulls: {nulls}/{i}, correct: {correct}/{i}\nTARGET BOARD: \n{boardToString(_board)} \n\nSEEN BOARD: \n{boardToString(newScan)}" +
+    //            $"\nREAL BOARD: \n{boardToString(game_instance.board)}");
+    //        // DEBUG
+
+    //        yield return new WaitForSeconds(DELAY_PER_SCAN);
+    //    }
+
+    //    // if 20% were nulls, or less than 80% of the scans matched,
+    //    // ignore this new board state. the player is probably making a move
+    //    // if (1.0f * nulls / NUMBER_OF_SCANS > 0.2f || 1.0f * correct / NUMBER_OF_SCANS < 0.7f)
+    //    if (1.0f * correct / NUMBER_OF_SCANS < 0.3f)
+    //    {
+    //        readingCoroutineActive = false;
+    //        print($"many scans failed. Nulls: {1.0f * nulls / NUMBER_OF_SCANS > 0.2f}, Matched enough: {1.0f * correct / NUMBER_OF_SCANS < 0.7f}");
+    //        yield break;
+    //    }
+
+    //    // board is confirmed to have made this move. "_board" is the current board state.
+    //    bool[,] errors = game_instance.GetErrors(game_instance.board, _board, game_instance.currentPlayer);
+    //    board_visuals.UpdateErrors(errors);
+
+
+    //    if (game_instance.ThereAreErrors(errors))
+    //    {
+    //        // let the player know they messed up
+    //        illegalMoveIndicator.DOLocalMoveY(50, 0.3f).SetEase(Ease.OutExpo);
+    //    }
+    //    else
+    //    {
+    //        // if illegal move indicator was shown on screen, bring it back down. at this point, all tiles are legal.
+    //        if (illegalMoveIndicator.localPosition.y > 0) illegalMoveIndicator.DOLocalMoveY(-50, 0.3f).SetEase(Ease.InExpo);
+
+    //        // advance the game!
+    //        Tuple<int, int, Side> pieceAdded = game_instance.GetFirstNewPiece(_board);
+    //        game_instance.SetTile(pieceAdded.Item1, pieceAdded.Item2, pieceAdded.Item3);
+
+    //        // IF WE GOT THE BONUS
+    //        if (game_instance.bonusLoc != null && pieceAdded.Item1 == game_instance.bonusLoc.Item1 && pieceAdded.Item2 == game_instance.bonusLoc.Item2)
+    //        {
+    //            AudioPlayer.instance.PlaySound("bonus");
+    //            board_visuals.EmitBonusParticles();
+    //            board_visuals.HideBonus();
+    //        }
+
+    //        int old_x = game_instance.x_score;
+    //        int old_o = game_instance.o_score;
+
+    //        int fiveInARows;
+    //        bool celebrateFiveInARow = false;
+
+    //        game_instance.x_score = game_instance.GetScore(Side.X, _board, true, out fiveInARows);
+    //        if (game_instance.x_fiveInARows < fiveInARows)
+    //        {
+    //            game_instance.x_fiveInARows = fiveInARows; celebrateFiveInARow = true;
+    //        }
+
+    //        game_instance.o_score = game_instance.GetScore(Side.O, _board, true, out fiveInARows);
+    //        if (game_instance.o_fiveInARows < fiveInARows)
+    //        {
+    //            game_instance.o_fiveInARows = fiveInARows; celebrateFiveInARow = true;
+    //        }
+
+    //        // IF SOMEONE GETS SCORE, DO THIS:
+    //        if (old_x < game_instance.x_score || old_o < game_instance.o_score)
+    //        {
+    //            int diff = Mathf.Max(game_instance.x_score - old_x, game_instance.o_score - old_o);
+    //            if (crAb != null) { crAb.intensity.value = 1f; DOTween.To(() => crAb.intensity.value, (float v) => { crAb.intensity.value = v; }, 0f, 1f).SetEase(Ease.OutCubic); }
+    //            if (paPro != null) { paPro.distance.value = 0.2f; DOTween.To(() => paPro.distance.value, (float v) => { paPro.distance.value = v; }, 0f, 1f).SetEase(Ease.OutCubic); }
+
+    //            // ADD POINT GAINED INDICATOR ON TOP OF PIECE 
+    //            board_visuals.SpawnPointsAbovePiece(pieceAdded.Item1, pieceAdded.Item2, diff);
+
+    //            // change bg color based on score received
+    //            if (diff >= 250)
+    //            {
+    //                gameCam.backgroundColor = Color.cyan;
+    //                gameCam.DOColor(Color.yellow, 0.4f).OnComplete(() =>
+    //                    gameCam.DOColor(Color.red, 0.4f).OnComplete(() => gameCam.DOColor(defaultCamColor, 0.4f))
+    //                );
+    //            }
+    //            else if (diff >= 150)
+    //            {
+    //                gameCam.backgroundColor = Color.yellow;
+    //                gameCam.DOColor(Color.red, 0.4f).OnComplete(() => gameCam.DOColor(defaultCamColor, 0.4f));
+    //            }
+    //            else if (diff >= 100)
+    //            {
+    //                gameCam.backgroundColor = Color.red;
+    //                gameCam.DOColor(defaultCamColor, 0.4f);
+    //            }
+    //            else
+    //            {
+    //                gameCam.backgroundColor = Color.gray;
+    //                gameCam.DOColor(defaultCamColor, 0.4f);
+    //            }
+    //        }
+
+    //        // IF SOMEBODY GOT A FIVE IN A ROW:
+    //        if (celebrateFiveInARow)
+    //        {
+    //            AudioPlayer.instance.PlaySound("bell");
+    //        }
+
+
+
+    //        game_instance.AdvanceTurn(); // update backend to make it the next player's turn
+
+    //        // UPDATE VISUALS
+    //        board_visuals.SetPiece(pieceAdded.Item1, pieceAdded.Item2, pieceAdded.Item3);
+    //        board_visuals.UpdateBonusLocation();
+
+    //        // UPDATE TEXT
+    //        turn_text.text = game_instance.currentPlayer == Side.X ? "It's X's turn!" : "It's O's turn!";
+
+
+    //        x_score_text.text = $"X SCORE: {game_instance.x_score}";
+    //        o_score_text.text = $"O SCORE: {game_instance.o_score}";
+    //    }
+
+
+    //    // if game over
+    //    if (game_instance.gameOver)
+    //    {
+    //        // handle visual stuff here
+    //        turn_text.text = "GAME OVER!";
+    //        gameObject.SetActive(false);
+    //    }
+
+    //    readingCoroutineActive = false;
+    //}
+
+
+
+    /// <summary>
+    /// pass in a new board state as a parameter, this function will certify
+    /// beyond a shadow of a doubt that the board is indeed this state, and make that move.
+    /// </summary>
+    /// <param name="board"></param>
+    /// <returns></returns>
+    private async Task ConfirmBoardStateAsync(Side[,] _board)
     {
         readingCoroutineActive = true;
 
-        // confirm board state
+        //confirm board state
         int correct = 0;
         int nulls = 0;
+
         for (int i = 0; i < NUMBER_OF_SCANS; i++)
         {
-
-            //run getBoardStateAsync as a task
-            Task<Side[,]> task = network_instance.GetBoardStateAsync();
-
-            //while getBoardStateAsync is still running wait here for another frame --> yield time back to the main thread (doesn't block)
-            while (!task.IsCompleted)
-                yield return null;
-            Side[,] newScan = task.Result;
-
+            Side[,] newScan = null;
+            try
+            {
+                // will wait here until GetBoardStateAsync() returns --> will not block main thread
+                newScan = await network_instance.GetBoardStateAsync();
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning($"Scan {i} failed: {e}");
+            }
 
             if (newScan == null) nulls++;
             else if (AreBoardsEqual(newScan, _board)) correct++;
 
             print($"nulls: {nulls}/{i}, correct: {correct}/{i}\nTARGET BOARD: \n{boardToString(_board)} \n\nSEEN BOARD: \n{boardToString(newScan)}" +
-                $"\nREAL BOARD: \n{boardToString(game_instance.board)}");
-            // DEBUG
+                  $"\nREAL BOARD: \n{boardToString(game_instance.board)}");
 
-            yield return new WaitForSeconds(DELAY_PER_SCAN);
+            await Task.Delay(TimeSpan.FromSeconds(DELAY_PER_SCAN));
         }
+
 
         // if 20% were nulls, or less than 80% of the scans matched,
         // ignore this new board state. the player is probably making a move
@@ -144,13 +355,13 @@ public class GameFlow : MonoBehaviour
         {
             readingCoroutineActive = false;
             print($"many scans failed. Nulls: {1.0f * nulls / NUMBER_OF_SCANS > 0.2f}, Matched enough: {1.0f * correct / NUMBER_OF_SCANS < 0.7f}");
-            yield break;
+            return;
         }
+
 
         // board is confirmed to have made this move. "_board" is the current board state.
         bool[,] errors = game_instance.GetErrors(game_instance.board, _board, game_instance.currentPlayer);
         board_visuals.UpdateErrors(errors);
-
 
         if (game_instance.ThereAreErrors(errors))
         {
@@ -192,12 +403,21 @@ public class GameFlow : MonoBehaviour
                 game_instance.o_fiveInARows = fiveInARows; celebrateFiveInARow = true;
             }
 
+
             // IF SOMEONE GETS SCORE, DO THIS:
             if (old_x < game_instance.x_score || old_o < game_instance.o_score)
             {
                 int diff = Mathf.Max(game_instance.x_score - old_x, game_instance.o_score - old_o);
-                if (crAb != null) { crAb.intensity.value = 1f; DOTween.To(() => crAb.intensity.value, (float v) => { crAb.intensity.value = v; }, 0f, 1f).SetEase(Ease.OutCubic); }
-                if (paPro != null) { paPro.distance.value = 0.2f; DOTween.To(() => paPro.distance.value, (float v) => { paPro.distance.value = v; }, 0f, 1f).SetEase(Ease.OutCubic); }
+                if (crAb != null)
+                {
+                    crAb.intensity.value = 1f;
+                    DOTween.To(() => crAb.intensity.value, (float v) => { crAb.intensity.value = v; }, 0f, 1f).SetEase(Ease.OutCubic);
+                }
+                if (paPro != null)
+                {
+                    paPro.distance.value = 0.2f;
+                    DOTween.To(() => paPro.distance.value, (float v) => { paPro.distance.value = v; }, 0f, 1f).SetEase(Ease.OutCubic);
+                }
 
                 // ADD POINT GAINED INDICATOR ON TOP OF PIECE 
                 board_visuals.SpawnPointsAbovePiece(pieceAdded.Item1, pieceAdded.Item2, diff);
@@ -227,13 +447,12 @@ public class GameFlow : MonoBehaviour
                 }
             }
 
+
             // IF SOMEBODY GOT A FIVE IN A ROW:
             if (celebrateFiveInARow)
             {
                 AudioPlayer.instance.PlaySound("bell");
             }
-
-
 
             game_instance.AdvanceTurn(); // update backend to make it the next player's turn
 
@@ -243,12 +462,9 @@ public class GameFlow : MonoBehaviour
 
             // UPDATE TEXT
             turn_text.text = game_instance.currentPlayer == Side.X ? "It's X's turn!" : "It's O's turn!";
-
-
             x_score_text.text = $"X SCORE: {game_instance.x_score}";
             o_score_text.text = $"O SCORE: {game_instance.o_score}";
         }
-
 
         // if game over
         if (game_instance.gameOver)
@@ -260,6 +476,11 @@ public class GameFlow : MonoBehaviour
 
         readingCoroutineActive = false;
     }
+
+
+
+
+
 
 
     // DEBUG
--- UNITY/Assets/Scripts/TitleScreenButtonScripts.cs
+++ UNITY/Assets/Scripts/TitleScreenButtonScripts.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using static GameState;
 using System.Collections;
+using UnityEngine.InputSystem;
 
 public class TitleScreenButtonScripts : MonoBehaviour
 {
@@ -57,56 +58,60 @@ public class TitleScreenButtonScripts : MonoBehaviour
 
 
 
-    private IEnumerator GetBoardCoroutine()
-    {
-        Task<Side[,]> task = Client.network_instance.GetBoardStateAsync();
-
-        //while getBoardStateAsync is still running wait here for another frame --> yield time back to the main thread (doesn't block)
-        while (!task.IsCompleted)
-            yield return null;
-
-        Side[,] newBoard = task.Result;
-
-        if (Client.network_instance.notConnected) networkErrorText.SetActive(true);
-        else networkErrorText.SetActive(false);
-
-
-        if (newBoard == null)
-        {
-            boardText.text = "Board not\nfound";
-        }
-        else
-        {
-            string str = "";
-            for (int y = 0; y < 5; y++)
-            {
-                for (int x = 0; x < 5; x++)
-                {
-                    str += newBoard[y, x] == Side.X ? "X " : newBoard[y, x] == Side.O ? "O " : "_ ";
-                }
-                str = str.TrimEnd();
-                str += '\n';
-            }
-            boardText.text = str;
-        }
-    }
-
-
-
-    // update calibration screen w/ what the camera sees
     private float clock = 0;
     public bool enableScanning = false;
+    private bool isChecking = false;
+
     private void Update()
     {
         if (!enableScanning) return;
-        clock += Time.deltaTime; // only increase clock counter if we're currently not confirming the board state
-        if (clock > 1) // every second, see if the read board state is different
+
+        clock += Time.deltaTime;
+        if (clock > 1f)
         {
             clock = 0;
-            //Side[,] newBoard = Client.network_instance.getBoardState();
-            StartCoroutine(GetBoardCoroutine());
-            //Side[,] newBoard = null;      
+            if (!isChecking)
+                _ = CheckBoardLoopAsync();
         }
     }
+
+
+    private async Task CheckBoardLoopAsync()
+    {
+        isChecking = true;
+        try
+        {
+            Side[,] newBoard = await Client.network_instance.GetBoardStateAsync();
+            if (Client.network_instance.notConnected) networkErrorText.SetActive(true);
+            else networkErrorText.SetActive(false);
+
+
+            if (newBoard == null)
+            {
+                boardText.text = "Board not\nfound";
+            }
+            else
+            {
+                string str = "";
+                for (int y = 0; y < 5; y++)
+                {
+                    for (int x = 0; x < 5; x++)
+                    {
+                        str += newBoard[y, x] == Side.X ? "X " : newBoard[y, x] == Side.O ? "O " : "_ ";
+                    }
+                    str = str.TrimEnd();
+                    str += '\n';
+                }
+                boardText.text = str;
+            }
+
+        }
+        finally
+        {
+            isChecking = false;
+        }
+    }
+
+
 
 }
